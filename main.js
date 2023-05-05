3e23279230fc19ac0c7dd3c241808f484f5f3433
Codey <1307>
2023-05-05 19:41:59 +0300

    hw1

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 //
-name = "Dmytro";
-admin = name;
+let name = "Dmytro";
+let admin = name;
 console.log(admin);
 //
-days = +prompt("Введіть число від 1 до 10:");
-seconds = days * 24 * 60 * 60;
+let days = +prompt("Введіть число від 1 до 10:");
+let seconds = days * 24 * 60 * 60;
 console.log(seconds);
 //
 console.log(prompt("Напишіть щось:"))
+
+console.log("3" + 3)
