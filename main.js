a78cb5387e8c4f26f9d1bc143c0f4fbbe7af0a00
Codey <1307>
2023-05-05 19:41:38 +0300

    hw1

---
+++ main.js
@@ -0,0 +1,10 @@
+//
+name = "Dmytro";
+admin = name;
+console.log(admin);
+//
+days = +prompt("Введіть число від 1 до 10:");
+seconds = days * 24 * 60 * 60;
+console.log(seconds);
+//
+console.log(prompt("Напишіть щось:"))
